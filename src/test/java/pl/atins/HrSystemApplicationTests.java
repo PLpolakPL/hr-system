0ff3ae79656881ba46719e7b89b8b0ee09dc6f16
Kamil Racko <s7871@atins.pl>
2025-07-26 17:47:48 +0200

    Initial commit with SpringBoot project structure

---
+++ src/test/java/pl/atins/HrSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.atins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
